6045e8d09e93eee1b0c834da0d4efa870211fa18
RupaliHangekar <RupaliHangekar@users.noreply.github.com>
2017-06-15 19:02:55 +0000

    Done

--- build.py
+++ build.py
@@ -1,2 +1,14 @@
 def solution(list_of_nums):
-    """Enter Code Here"""
+    d={}
+    ce=0
+    co=0
+    for i in list_of_nums:
+        if i%2==0:
+            ce=ce+1
+        else:
+            co=co+1
+    d.update({'ODD':co})
+    d.update({'EVEN':ce})
+    return d
+
+print(solution([1, 2, 3, 5, 8, 9]))
